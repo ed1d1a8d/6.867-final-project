515560aa4c33007b692c5096d3b312fada4705e4
ed1d1a8d <tong.t.wang@gmail.com>
2017-12-04 00:02:08 +0000

    Update spectrogram util

--- notebooks/utils/spectrogram.py
+++ notebooks/utils/spectrogram.py
@@ -1,23 +1,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy import signal
-from scipy.io import wavfile
+import librosa
+import librosa.display
 
-def get_data(fp):
-    samprate, data = wavfile.read(fp)
-    assert samprate == 16000
-    assert len(data.shape) == 1
-    assert data.dtype == np.dtype('int16')
-    return samprate, data
+def get_spectrogram(audio_path):
+    y, sr = librosa.load(audio_path, sr=16000)
+    S = librosa.feature.melspectrogram(y, sr=sr, n_mels=128, n_fft=512, hop_length=128)
+    log_S = librosa.power_to_db(S, ref=np.max)
+    mn = np.min(log_S)
+    mx = np.max(log_S)
+    log_S -= mn
+    log_S /= (mx - mn)
+    return log_S
 
-def spectrogram(fp, period_secs = 0.5):
-    sample_rate, samples = get_data(fp)
-    frequencies, times, spectrogram = signal.spectrogram(samples, period_secs*sample_rate)
-    return frequencies, times, spectrogram
+def show_spectrogram(audio_path):
+    _, sr = librosa.load(audio_path, sr=16000)
+    log_S = get_spectrogram(audio_path)
 
-if __name__ == '__main__':
-    fp = 'Katey_Sagal/4HvcwvtDLKw_0000001.wav'
-    frequencies, times, spectrogram = spectrogram(fp)
-    plt.imshow(spectrogram, aspect='auto', cmap='hot_r', origin='lower')
-    plt.show()
-
+    plt.figure(figsize=(12,4))
+    librosa.display.specshow(log_S, sr=sr, hop_length=128, x_axis='time', y_axis='mel')
+    plt.title('mel power spectrogram')
+    plt.colorbar(format='%+02.0f dB')
