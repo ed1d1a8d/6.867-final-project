882b0fa862cebf4ae4c896b3e3f1e8a8b00147c4
Sanjeev Murty <smurty@mit.edu>
2017-12-05 23:38:08 -0500

    put code for getting clip paths into function

--- get_data.py
+++ get_data.py
@@ -1,26 +1,32 @@
 import numpy as np
-
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import librosa
-import librosa.display
+#import librosa.display
 
 import os, sys
 
-# Open a file
 VCB_PATH = "/home/ubuntu/data/voxceleb1_wav"
-names = [d for d in os.listdir(VCB_PATH) if os.path.isdir(os.path.join(VCB_PATH, d))]
+def get_names_and_clips():
+    global VCB_PATH
+    names = [d for d in os.listdir(VCB_PATH) if os.path.isdir(os.path.join(VCB_PATH, d))]
 
-audio_clips = {}
-for name in names:
-    audio_clips[name] = os.listdir(VCB_PATH + '/' + name)
+    audio_clips = {}
+    for name in names:
+        audio_clips[name] = os.listdir(VCB_PATH + '/' + name)
 
-names = sorted(names, key=lambda name : -len(audio_clips[name]))
-print(names[:10])
-print(audio_clips[names[0]])
+    names = sorted(names, key=lambda name : -len(audio_clips[name]))
+
+    return names, audio_clips
+    
+    #print(names[:10])
+    #print(audio_clips[names[0]])
 
 
-audio_path = '/'.join([VCB_PATH, names[0], audio_clips[names[0]][0]])
-print(audio_path)
+    #audio_path = '/'.join([VCB_PATH, names[0], audio_clips[names[0]][0]])
+    #print(audio_path)
+    
+
+
 
 
 
